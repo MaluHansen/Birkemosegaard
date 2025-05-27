ed233ee80ad2cd100544539104dd8add3e33af33
AlexMoldt <alexmoldt@outlook.com>
2025-05-27 10:29:38 +0200

    laved sekktion til handel

---
+++ assets/js/Handelsbetingelser.js
@@ -0,0 +1,6 @@
+document.querySelectorAll(".accordion-toggle").forEach((button) => {
+  button.addEventListener("click", () => {
+    const accordion = button.parentElement;
+    accordion.classList.toggle("open");
+  });
+});
